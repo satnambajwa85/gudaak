5bafd93a7ad7c3a5519ea784fd053aa14e27e4f4
Jagraj Singh <jagraj2007@hotmail.com>
2014-02-26 18:51:28 +0530

    some user compelete profile changes

--- protected/components/WidgetDashboardMenu.php
+++ protected/components/WidgetDashboardMenu.php
@@ -21,17 +21,27 @@ class WidgetDashboardMenu extends CWidget
    protected function renderContent()
    	{	$completeProfile	='';
 	 	$userProfile	=	UserProfiles::model()->findByPK(Yii::app()->user->profileId);
+		$userProfileProcess	=	UserProfilesHasUserSubjects::model()->countByAttributes(array('user_profiles_id'=>Yii::app()->user->profileId)); 
+		$userProfileProcess2	=UserProfilesHasInterests::model()->countByAttributes(array('user_profiles_id'=>Yii::app()->user->profileId)); 
 		$tests			=	OrientItems::model()->findAllByAttributes(array('published'=>1,'status'=>1),array('order'=>'title ASC '));
 		$userTest	 	=  UserReports::model()->countByAttributes(array('user_profiles_id'=>Yii::app()->user->profileId)); 
+		 
 		if($userTest==1){
 			$completeProfile	=	'50%'; 
 		}
 		if($userTest==2){
 			$completeProfile	=	'100%'; 
 		}
-		if($userTest==0 || $userTest== ''){
+		if($userProfileProcess>=1 && $userTest==0){
 			$completeProfile	=	'10%'; 
 		}
+		if($userProfileProcess2>=1 && $userTest==0){
+			$completeProfile	=	'20%'; 
+		}
+		if(empty($userProfileProcess)){
+			$completeProfile	=	'0%'; 
+		}
+		
 		
 		
 		$this->render('widgetDashboardMenu',array('userinfo'=>$userProfile,'tests'=>$tests,'completeProfile'=>$completeProfile));
