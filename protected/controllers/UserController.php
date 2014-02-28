6cef68120c7c9066c81d29c8ba99546cb2cbcc3b
Satnam singh <satnam.singh@venturepact.com>
2014-02-28 16:38:32 +0530

    changes

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -498,12 +498,12 @@ class UserController extends Controller
 				$score	=	$cur->score;
 				foreach($cur->careerCategories->careerAssessments as $asswssment){
 					if($score >= $asswssment->score_start && $score <= $asswssment->score_end){
-						$userTest[$asswssment->value][$cur->id]['value']		=	$asswssment->value;
-						$userTest[$asswssment->value][$cur->id]['score']		=	$score;	
-						$userTest[$asswssment->value][$cur->id]['id']			=	$cur->careerCategories->id;
-						$userTest[$asswssment->value][$cur->id]['title']		=	$cur->careerCategories->title;
-						$userTest[$asswssment->value][$cur->id]['title2']		=	$asswssment->title;
-						$userTest[$asswssment->value][$cur->id]['description']	=	$asswssment->description;
+						$userTest[$asswssment->value][$cur->career_categories_id]['value']		=	$asswssment->value;
+						$userTest[$asswssment->value][$cur->career_categories_id]['score']		=	$score;	
+						$userTest[$asswssment->value][$cur->career_categories_id]['id']			=	$cur->careerCategories->id;
+						$userTest[$asswssment->value][$cur->career_categories_id]['title']		=	$cur->careerCategories->title;
+						$userTest[$asswssment->value][$cur->career_categories_id]['title2']		=	$asswssment->title;
+						$userTest[$asswssment->value][$cur->career_categories_id]['description']=	$asswssment->description;
 					}
 				}
 				
@@ -535,14 +535,41 @@ class UserController extends Controller
 			$data[$report->orient_items_id]['results']=$final;
 		}else{
 				$final		=	array();
+				$final1		=	array();
+				if(isset($userTest['high'])){
+					$highCount	=	count($userTest['high']);
+					$final		=	$userTest['high'];
+					$final1		=	$userTest['high'];
+				}
+				if(isset($userTest['moderate'])){
+					$midCount	=	count($userTest['moderate']);
+					$final1		=	array_merge($final1,array_slice($userTest['moderate'], 0, 5));
+				}
+				
+				if($highCount ==	0 && isset($userTest['moderate']))
+					$final		=	array_merge($final,array_slice($userTest['moderate'], 0, 2));
+				if($highCount>0 && $highCount < 2 && isset($userTest['moderate']))
+					$final		=	array_merge($final,array_slice($userTest['moderate'], 0, 1));
+				if(isset($userTest['low']))
+					$final1		=	array_merge($final1,array_slice($userTest['low'], 0, 5));
+				$total	=	$highCount+$midCount;
+				//Sorting accounding to category id
+				ksort($final1);
+				ksort($final);
+				$data[$report->orient_items_id]['results1']=$final1;
+				$data[$report->orient_items_id]['results']=$final;
+			}else{
+				$final		=	array();
+				
 				if(isset($userTest['high'])){
 					$final		=	$userTest['high'];
 				}
 				if(isset($userTest['moderate']))
-					$final		=	array_merge($final,array_slice($userTest['moderate'],0,5));
+					$final		=	$final+$userTest['moderate'];
+					
 				if(isset($userTest['low']))
-					$final		=	array_merge($final,array_slice($userTest['low'],0,5));
-				
+					$final		=	$final+$userTest['low'];
+				ksort($final);
 				$data[$report->orient_items_id]['results']=$final;
 			}
 			
@@ -551,9 +578,6 @@ class UserController extends Controller
 		
 		
 		if(isset($_REQUEST['download']) && $_REQUEST['download']==1){
-			//ob_clean(); 
-			
-			
 			$html2pdf = Yii::app()->ePdf->HTML2PDF();
 			ob_end_clean();
 			if(Yii::app()->user->userType ==  'below10th')
