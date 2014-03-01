eb1d73d9c1e12f6d5442a2a5f110d18dd66bef02
Satnam singh <satnam.singh@venturepact.com>
2014-03-01 13:06:44 +0530

    changes

--- themes/template/views/user/detailedReport2.php
+++ themes/template/views/user/detailedReport2.php
@@ -16,12 +16,38 @@ $this->breadcrumbs=array('Detailed Report'=>array('/user/detailedReport'));?>
 			} ?>
 			</div>
 		<div class="col-md-12 pull-left br-all inner-padding">
-			<div id="scrollBar" style="max-height:490px">
-			<div class="col-md-10 col-sm-offset-1 pull-left  pd0">
-					<div class="col-md-12 fl details-report">
-						<h1>Profile Summary</h1>
-					</div>
-					<div class="col-md-12  pull-left report-border pd0">
+			<div id="scrollBar" style="max-height:490px;width:97%;">
+			<div class="wrapper">
+			<div class="summary-report-title1">Profile Summary&nbsp;
+				<div class="report-log-img"><img alt="" src="./image/small.jpg"></div>                    
+			</div>
+    				<div class="w100" >
+                	<div class="report-title1">
+                    	Gudaak IPC Report
+                    </div>
+                	<div style="float:left; width:100%; font-size:14px; color:#666; text-align:justify; line-height:22px; font-family:, Helvetica, sans-serif;  ">
+                    	<p>Welcome to the world of Designing Career Path with Gudaak! The test you have taken just now is one of the numerous “Interest-Personality “Tests available online and offline but our uniqueness lies in the interpretation. We at Gudaak believe in the holistic approach towards life and careers. This report has been prepared with utmost care, keeping in mind the various factors that come into play when you are making a decision for life.<br/><br/>
+Any suitable career can only be predicted with a combination of client’s academic assessments, interests, personality, values and attitudes when assessed in congenial environment. So, to succeed in life- Keep reading the literature on career and explore the possibilities that can help you reach your dreams and goals.<br/><br/>
+The career recommendations made here are based on how genuinely you have answered the test. We hope that this report will lead to a greater understanding of yourself and at every step we are there to guide you.<br/><br/>
+Remember! “There are no such things as limits to growth, because there are no limits on the human capacity for intelligence, imagination and wonder."<br/><br/>
+
+Wishing you all the best in all your future endeavours!
+<br/>
+                        </p>
+						<div class="def-title">This report contains the following:<br/>
+								<ul class="def-list">
+								<li>• A personalized personality profile</li>
+								<li>• An interest profile</li>
+								<li>• Career Recommendations</li>
+								</ul>
+
+						</div>
+                    </div>	
+               <div style="float:left; width:90%; background:url(./image/img_icon.jpg) no-repeat;height:129px; background-position:center; margin-top:8%; margin-bottom:8%;"></div>
+                
+                
+                </div>	
+<div class="user-report-info  pull-left report-border pd0">
 						<div class="col-md-6  pull-left left-section pd0">
 							<ul>
 								<li>Name</li>
@@ -45,41 +71,103 @@ $this->breadcrumbs=array('Detailed Report'=>array('/user/detailedReport'));?>
 						<div class="col-md-6  pull-left">
 						</div>
 					</div>
-					<div class="col-md-12">
-                       <div class="clear"></div>
-                       <br /><br />
-                        <?php foreach($reports as $report){?>
-                        <div class="row">
-							<ul id="report-bottom-area">
-									<li>
-										<h1><?php echo $report['name'];?></h1>
-										<p><?php echo $report['description'];?></p>
-									</li>
-									
-									
-							</ul>
-                           
-						</div>
-                        <div class="clear"></div>
-                        <?php if($report['id']==3){?>
-<div class="sec1">
-	<?php foreach($report['results1'] as $result){?>
-    <div>
-		<div class="w20 fl"><?php echo $result['title'];?></div>
-		<div class="w80 progress<?php echo $result['id'];?>"><span style="width:<?php echo ($result['score']/0.4);?>%"></span></div>
-	</div>
-    <?php  }?>
-</div>
+ 	
+        
+ <div style="float:left; width:100%; min-height:700px; " >
 
-                        <?php } 
+ 
+              
+                <div style="float:left; width:100%;" >
+				
+	                  <?php foreach($reports as $report){?>
+						<?php if($report['id']==2){ ?>
+						<div class="w100" >
+						<div class="summary-report-title">
+							Personality Test Summary 
+						</div>
+							
+							<h1 class="report-title2">
+								Your Personality Mapping
+							</h1>
+							<div class="clear"></div>
+							<div class="r-description">
+								<p style="font-weight:bold;">Personality:  What a dynamic "Personality" she has! You have often heard people saying this. Is it possible to assess the personality of a human being just by looking at him/her? Actually, the answer could be both-Yes and No. Yes, because outward appearance comes from the confidence, self-esteem and emotional stability you have and No because people might fake it.
+
+								</p>
+								<p>Personality as defined by researchers is a dynamic and organized set of characteristics possessed by a person that uniquely influences his or her cognitions, emotions, motivations, and behaviours in various situations. It also refers to the pattern of thoughts, feelings, social adjustments, and behaviours consistently exhibited over time that strongly influences one's expectations, self-perceptions, values, and attitudes. It also predicts human reactions to other people, problems, and stress.			</p>
+								<p style="font-size:18px;color:#666;padding:0;margin:0;">
+									This section of the report will show you how you stack up on 5 major dimensions of personality:
+								</p>
+								<br/>
+								<br/>
+								<p>
+									•	<span style="font-weight:bold;">Openness to Experience -</span> How much do you enjoy abstract ideas and artistic expression?<br/><br/>
+									•	<span style="font-weight:bold;">Conscientiousness -</span> How much do you put off immediate gratification in order to achieve long-term goals?<br/><br/>
+									•	<span style="font-weight:bold;">Extraversion -</span> How much do you turn to the outside world for stimulation and excitement?<br/><br/>
+									•	<span style="font-weight:bold;">Agreeableness -</span> How much do you put others ahead of yourself?<br/><br/>
+									•	<span style="font-weight:bold;">Neuroticism - </span>How likely are you to bounce back from stressful events?<br/><br/>
+
+
+								</p>
+							</div>	
+					   
+						
+						
+						</div>
+						<?php } ?>
+					
+					 
+                        <?php if($report['id']==3){?>
+						   
+							<div style="margin-top:50px;">
+								<div style="w100" >
+									<div class="summary-report-title">
+										Interest Test Summary 
+									</div>
+									<h1 class="report-title2">
+										Your Personality Mapping
+									</h1>
+									<div class="clear"></div>
+									<div class="r-description">
+										<p style="font-weight:bold;">
+										 Interests in a layman language can be “What you like to do?” You might like swimming, reading or travelling and may not like fishing, meeting people etc. It refers to things or activities that a person is curious or concerned about. They are subjects or qualities that evoke his attention. They can be things that someone does as a pastime or a leisure activity or those that he wants to have an occupation in. There are various interests that a person might be enthusiastic about.  Do not worry if the results here are not what you thought of. Our idea is to let you know your real self!
+
+										</p>
+										<p>The Interest Test you have taken maps you on the five traits. Read out in details here:<br/><br/><br/></p>
+										 
+										
+							
+									</div>	
+							   
+								
+								
+								</div>
+						 <div class="user-report-info" >
+							<?php foreach($report['results1'] as $result){?>
+								     <div >
+										<div class="w20 fl process-title"><?php echo $result['title'];?></div>
+										<div class="w80 progress<?php echo $result['id'];?>"><span style="width:<?php echo ($result['score']/0.4);?>%"></span></div>
+									</div>
+															 
+							 <?php  }?>
+							  </div >
+							  
+							</div>
+
+                        <?php } ?>
                         
- foreach($report['results'] as $result){?>
-	<div class="">
-		<div class="clear"></div>
-		<?php echo ($report['id']==3)?'<h4>'.$result['title'].'</h4>':'<h4>'.$result['title2'].'</h4>';?>
-		<p class="description-content"><?php echo $result['description'];?></p>
+ <?php foreach($report['results'] as $result){?>
+		     <div class="cat-align">
+				<?php echo ($report['id']==3)?'<h3 class="process-title">'.$result['title'].'</h3>':'<h3 class="process-title">'.$result['title2'].'</h3>';?>
+			</div>
+			<div class="cat-def">
+			<p><?php echo $result['description'];?></p>
+			
+			</div>	
+		
+		<p class="description-content"></p>
     	<div class="border_b"></div>
-	</div>
+	 
     <?php 
 	} ?><div>
     <?php
@@ -107,7 +195,7 @@ $this->breadcrumbs=array('Detailed Report'=>array('/user/detailedReport'));?>
 			
 			
 			?>
-	<div class="col-md-4 pd0  fl">				
+	<div class="col-md-3 pd0  fl">				
 	<div class="col-md-12 pdleft fl career-lib">
 	<?php 
 			$filename = ''.$streamRec->image.'';
@@ -121,7 +209,7 @@ $this->breadcrumbs=array('Detailed Report'=>array('/user/detailedReport'));?>
 	<?php } ?>
 	<div class="clear"></div>
 	<?php echo CHtml::link('<h1>'.substr($streamRec->name,0,20).'..</h1>',array('user/stream','id'=>''.$streamRec->id.''),array('title'=>$streamRec->name));?>
-	<p><?php echo substr($streamRec->description,0,70);?></p>
+	<div style="  min-height: 14px !important;"><?php echo substr($streamRec->description,0,70);?></div>
 	<div class="col-md-12 career-hot-links">
 	<?php echo CHtml::link('Read more..',array('user/readFull','id'=>''.$streamRec->id.''),array('class'=>'pull-left','title'=>'Read Full.'));?>
 		<span class="pull-right"><i class="icon-eye-open"></i></span>
@@ -141,18 +229,39 @@ $this->breadcrumbs=array('Detailed Report'=>array('/user/detailedReport'));?>
         </div>
         <div class="clear"></div>
 <?php } ?>
-		</div>
+				</div>    
+                </div>
+ 
+ 			 
+     
+    <!--Page NO. 1 End-->
+    </div>
+ 
+      <div class="wrapper">
+      
+			 	<div style="w100" >
+                     <div class="r-footer"  >
+                    	<div style="text-align:center; margin-right:2%; color:#fff; font-size:17px; line-height:22px; font-family:Arial, Helvetica, sans-serif; margin-top:7%;">
+                        	
+                        Call: +91 8786 76545, +91 7654 763592 <br/>
+                        Email: info@gudaak.com &nbsp;&nbsp; Website: www.gudaak.com 
+                        </div>
+                    
+                  </div>
+ 
+ 				
+     
+    <!--Page NO. 1 End-->
+    </div>
+    
+		
+			</div>
 					
-				
-			</div>
-			
-			
-			</div>
 			</div>
 		
+		</div>
 		</div>
         
-		
 		
 
 
