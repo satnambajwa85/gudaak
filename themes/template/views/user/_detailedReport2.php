320d438214beb22cc56da5b329bc512d0b96683e
Jagraj Singh <jagraj2007@hotmail.com>
2014-03-01 17:51:49 +0530

    update from FTP files

--- themes/template/views/user/_detailedReport2.php
+++ themes/template/views/user/_detailedReport2.php
@@ -12,6 +12,11 @@
 				</p>
 
 			</div>
+			<?php if(isset($reports)){?>
+					<?php echo CHtml::link('Download PDF',array('user/DetailedReport&download=1'),array('target'=>'_blank','class'=>'btn btn-info2 center-bt fr'))?>
+			<?php }else{
+			
+			} ?>
 			
 		</div>
 		<div class="col-md-12 pull-left inner-padding">
@@ -159,6 +164,3 @@ foreach($report['results'] as $result){
  </div>       
 
 		
-
-
-	 
