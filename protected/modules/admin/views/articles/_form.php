661f11da778631eb6efc51b496129dc633124fbc
Satnam singh <satnam.singh@venturepact.com>
2014-03-19 14:44:49 +0530

    changes

--- protected/modules/admin/views/articles/_form.php
+++ protected/modules/admin/views/articles/_form.php
@@ -13,67 +13,78 @@
 	// There is a call to performAjaxValidation() commented in generated controller code.
 	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
+	'htmlOptions'=>array('enctype'=>'multipart/form-data'),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'title'); ?>
 		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>500)); ?>
 		<?php echo $form->error($model,'title'); ?>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'description'); ?>
-		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
+		<?php $this->widget('application.extensions.ckeditor.CKEditor', array(
+												'model'=>$model,
+												'attribute'=>'description',
+												'language'=>'en',
+												'editorTemplate'=>'full',
+												)); ?>
 		<?php echo $form->error($model,'description'); ?>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'add_date'); ?>
-		<?php echo $form->textField($model,'add_date'); ?>
+		<?php	$this->widget('zii.widgets.jui.CJuiDatePicker',array(
+									'model'=>$model,
+									'attribute'=>'add_date',
+									'options'=>array('dateFormat'=>'yy-mm-dd','minDate'=>0),
+									'htmlOptions'=>array('class'=>'form-control'),
+									'value'=>date('Y-m-d', strtotime('+2 day', strtotime(date('Y-m-d')))),
+									));?>
 		<?php echo $form->error($model,'add_date'); ?>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'image'); ?>
-		<?php echo $form->textField($model,'image',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->fileField($model,'image',array('size'=>45,'maxlength'=>45)); ?>
 		<?php echo $form->error($model,'image'); ?>
+		<?php if(isset($model->image)){ ?> 
+		<?php echo $form->hiddenField($model,'oldImage',array('value'=>$model->image)); ?>
+		<img width="100" height="100" src="<?php echo Yii::app()->request->baseUrl.'/uploads/articles/small/'.$model->image;?>" alt="image"/>
+		<?php }?>
 	</div>
-
-	<div class="row">
+	
+    
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'published'); ?>
-		<?php echo $form->textField($model,'published'); ?>
+		<?php echo $form->radioButtonlist($model,'published',array('1'=>'Yes','0'=>'No'),array('separator'=>'')); ?>
 		<?php echo $form->error($model,'published'); ?>
 	</div>
-
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
+		<?php echo $form->radioButtonlist($model,'status',array('1'=>'Yes','0'=>'No'),array('separator'=>'')); ?>
 		<?php echo $form->error($model,'status'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'user_login_id'); ?>
-		<?php echo $form->textField($model,'user_login_id'); ?>
-		<?php echo $form->error($model,'user_login_id'); ?>
-	</div>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'author'); ?>
 		<?php echo $form->textField($model,'author',array('size'=>60,'maxlength'=>255)); ?>
 		<?php echo $form->error($model,'author'); ?>
 	</div>
 
-	<div class="row">
+	<div class="form-group">
 		<?php echo $form->labelEx($model,'role'); ?>
 		<?php echo $form->textField($model,'role',array('size'=>60,'maxlength'=>255)); ?>
 		<?php echo $form->error($model,'role'); ?>
 	</div>
 
-	<div class="row buttons">
+	<div class="form-group buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
 
