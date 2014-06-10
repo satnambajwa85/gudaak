4bf73e020b9bd67b87c07e786c5dd0aa8b7bd7e9
Satnam singh <satnam.singh@venturepact.com>
2014-06-10 14:39:57 +0530

    changes

--- protected/modules/admin/views/session/admin.php
+++ protected/modules/admin/views/session/admin.php
@@ -44,9 +44,9 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'other',
 		'status',
 		array(
-		'type'=>'raw',
-		'name'=>'Questions',
-		'value'=>'CHtml::link("Questions",array("/admin/sessionQuestions/adminView","id"=>$data->id))',
+			'type'=>'raw',
+			//'name'=>'Questions',
+			'value'=>'CHtml::link("Questions",array("/admin/sessionQuestions/adminView","id"=>$data->id))',
 		),
 		array(
 			'class'=>'CButtonColumn',
