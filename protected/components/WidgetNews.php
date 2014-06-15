b425ddd131b9ee60d293a7ca2335ea9ae80f08fb
satnam <satnambajwa85@gmail.com>
2014-06-15 15:06:54 +0530

    changes in news order

--- protected/components/WidgetNews.php
+++ protected/components/WidgetNews.php
@@ -23,7 +23,7 @@ class WidgetNews extends CWidget
 	 	$criteria			=	new CDbCriteria();
 		$criteria->condition=	'(published =:published and status =:status )';
 		$criteria->params 	=	array('published'=>1,'status'=>1);
-		$criteria->order	=	'add_date ASC';
+		$criteria->order	=	'add_date DESC';
 			
 		$count				=	News::model()->count($criteria);
 		$pages				=	new CPagination($count);
