52e93f932bf017e95e3702778c43a3cc654cf8eb
Satnam singh <satnam.singh@venturepact.com>
2014-06-16 10:45:23 +0530

    changes

--- protected/components/views/widgetNews.php
+++ protected/components/views/widgetNews.php
@@ -3,10 +3,11 @@
 		 News
 	</div>
 	<div class="list-group">
-		<?php foreach($news as $list){ ?>
-		<?php echo CHtml::link('<h4 class="list-group-item-heading">'.$list->title.'</h4><p class="list-group-item-text">'.substr($list->description,0,55).'..</p>',array('user/news','id'=>$list->id),array('class'=>'list-group-item active','title'=>''.$list->title.''));?>
-		<?php } ?>
-		 <div class="col-md-12 pull-left news-pager">
+		<?php foreach($news as $list){
+			echo CHtml::link('<h4 class="list-group-item-heading">'.$list->title.'</h4>',array('user/news','id'=>$list->id),array('class'=>'list-group-item active','title'=>''.$list->title.''));
+			echo '<p class="list-group-item-text">'.substr($list->description,0,55).'..</p>';
+		}?>
+        <div class="col-md-12 pull-left news-pager">
 			<?php $this->widget('CLinkPager', array('pages' => $pages)) ?>
 		</div>
 		<!--<button type="button" class="btn btn-default btn-lg btn-block lodad-more">Load more news</button>-->
