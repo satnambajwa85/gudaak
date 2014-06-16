e19e5083c82cfb2ee384a5ddb4afa3ac33e7b403
Satnam singh <satnam.singh@venturepact.com>
2014-06-16 11:06:03 +0530

    changes

--- protected/components/views/widgetNews.php
+++ protected/components/views/widgetNews.php
@@ -4,12 +4,10 @@
 	</div>
 	<div class="list-group">
 		<?php foreach($news as $list){
-			echo CHtml::link('<h4 class="list-group-item-heading">'.$list->title.'</h4><p class="list-group-item-text">'.substr($list->description,0,55).'..</p>',array('user/news','id'=>$list->id),array('class'=>'list-group-item active','title'=>''.$list->title.''));
-			//echo '';
+			echo CHtml::link('<h4 class="list-group-item-heading">'.$list->title.'</h4><p class="list-group-item-text">'.substr(strip_tags($list->description),0,55).'..</p>',array('user/news','id'=>$list->id),array('class'=>'list-group-item active','title'=>''.$list->title.''));
 		}?>
         <div class="col-md-12 pull-left news-pager">
 			<?php //$this->widget('CLinkPager', array('pages' => $pages)) ?>
-		</div>
-		<!--<button type="button" class="btn btn-default btn-lg btn-block lodad-more">Load more news</button>-->
+		</div>		
 	  </div>
 </div>
