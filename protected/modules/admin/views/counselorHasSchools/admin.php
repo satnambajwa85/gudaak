8b03890b6186c64665d68d16b807498d87102761
Satnam singh <satnam.singh@venturepact.com>
2014-06-22 13:46:45 +0530

    changes

--- protected/modules/admin/views/counselorHasSchools/admin.php
+++ protected/modules/admin/views/counselorHasSchools/admin.php
@@ -55,7 +55,7 @@ else
 		array(
 			'type'=>'raw',
 			'name'=>'schools_id',
-            'value'=>'$data->schools->name($data->schools->userLogin->username)',
+            'value'=>'$data->schools->name($data->schools->schoolsHasUserLogins->userLogin->username)',
         ),
 		'service_type',
 		'add_date',
