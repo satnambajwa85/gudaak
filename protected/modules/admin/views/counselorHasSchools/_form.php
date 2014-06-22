a0db6fdea7adb40a9a3418f2f341b38ca1b50d45
Satnam singh <satnam.singh@venturepact.com>
2014-06-22 13:30:09 +0530

    changes

--- protected/modules/admin/views/counselorHasSchools/_form.php
+++ protected/modules/admin/views/counselorHasSchools/_form.php
@@ -6,9 +6,9 @@
 )); ?>
 <?php 
 if(isset($_REQUEST['counselor_id']))
-	echo CHtml::link("Counselor's School",array('/admin/counselorHasSchools/admin','counselor_id'=>$_REQUEST['counselor_id']),array('class'=>'pull-right btn btn-s-md btn-success mr20'));
+	echo CHtml::link("Back to Counselor's School",array('/admin/counselorHasSchools/admin','counselor_id'=>$_REQUEST['counselor_id']),array('class'=>'pull-right btn btn-s-md btn-success mr20'));
 else
-	echo CHtml::link("Counselor's School",array('/admin/counselorHasSchools/admin'),array('class'=>'pull-right btn btn-s-md btn-success mr20'));?>
+	echo CHtml::link("Back to Counselor's School",array('/admin/counselorHasSchools/admin'),array('class'=>'pull-right btn btn-s-md btn-success mr20'));?>
 
 	<?php echo $form->errorSummary($model); ?>
 
