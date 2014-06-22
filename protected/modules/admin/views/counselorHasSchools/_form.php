2f8154bfee6b574271d275e4b34fb98d7ca97ae8
Satnam singh <satnam.singh@venturepact.com>
2014-06-22 12:50:30 +0530

    dfsd

--- protected/modules/admin/views/counselorHasSchools/_form.php
+++ protected/modules/admin/views/counselorHasSchools/_form.php
@@ -11,7 +11,7 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'counselor_id'); ?>
-		<?php echo $form->textField($model,'counselor_id'); ?>
+		<?php echo  $form->dropDownList($model,'counselor_id',CHtml::listData(counselor::model()->findAll(),'id','first_name'.' last_name'),array('empty'=>'Please select'));?>
 		<?php echo $form->error($model,'counselor_id'); ?>
 	</div>
 
