c19608a3f72e8d7df25858944216dc71ef94fe9e
Satnam singh <satnam.singh@venturepact.com>
2014-06-22 12:13:53 +0530

    changes

--- protected/modules/admin/views/counselorHasSchools/admin.php
+++ protected/modules/admin/views/counselorHasSchools/admin.php
@@ -25,10 +25,7 @@ $('.search-form form').submit(function(){
 
 <h1>Manage Counselor Has Schools</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
+<?php echo CHtml::link('Back to Counselor list',array('/admin/Counselor/admin'),array('class'=>'pull-right btn btn-danger ui-slider'));?>
 
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
