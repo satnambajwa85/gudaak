28f4fdab4deb1626bd25c5324a9d1654be6c8d6d
Satnam singh <satnam.singh@venturepact.com>
2014-06-24 17:02:02 +0530

    changes

--- themes/template/views/site/article.php
+++ themes/template/views/site/article.php
@@ -19,6 +19,10 @@ $this->breadcrumbs=array('Articles'=>array('/site/articles'),''.$articles->title
 				<p>
 				<?php echo substr($articles->description,0,3000);?>	
 				</p>
+                
+                <?php $this->widget('ext.YiiDisqusWidget.YiiDisqusWidget',array('shortname'=>'gudaak'));?>
+                
+
 			</div>
 		</div>
 </div>
