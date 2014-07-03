c320f1bfd63cd1c34a58c7acfad3695ac13dc344
Satnam singh <satnam.singh@venturepact.com>
2014-07-03 12:16:22 +0530

    changes in news list

--- protected/components/views/widgetNews.php
+++ protected/components/views/widgetNews.php
@@ -1,7 +1,6 @@
 <div class="widget-body">
-	<div class="widget-heading blue">
-		 News
-	</div>
+	<?php echo CHtml::link('<div class="widget-heading blue">News</div>',array('user/newsList'));?>
+    
 	<div class="list-group">
 		<?php foreach($news as $list){
 			echo CHtml::link('<h4 class="list-group-item-heading">'.$list->title.'</h4><p class="list-group-item-text">'.substr(strip_tags($list->description),0,55).'..</p>',array('user/news','id'=>$list->id),array('class'=>'list-group-item active','title'=>''.$list->title.''));
