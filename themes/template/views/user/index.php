afae27ff54f42a4e304b23f71bb09067946c3517
Satnam singh <satnam.singh@venturepact.com>
2014-07-07 15:16:14 +0530

    changes

--- themes/template/views/user/index.php
+++ themes/template/views/user/index.php
@@ -1,30 +1,30 @@
 <?php 
 if(isset($_REQUEST['first']) && $_REQUEST['first']==1)
-	Yii::app()->clientScript->registerScript('opact','$("#popup_box").fadeIn("slow");$("#container").css({"opacity": "0.3" }); $("#popup_box").click(function(){$("#popup_box").fadeOut();});',CClientScript::POS_READY);
-	
+Yii::app()->clientScript->registerScript('opact','$("#popup_box").fadeIn("slow");$("#container").css({"opacity": "0.3" }); $("#popup_box").click(function(){$("#popup_box").fadeOut();$("#popup_box1").fadeIn("slow");$("#container").css({"opacity": "0.3" });});
+$("#popup_box1").click(function(){$("#popup_box1").fadeOut();});',CClientScript::POS_READY);
 $this->pageTitle=Yii::app()->name . ' - Tour';
 $this->breadcrumbs=array('Tour'=>array('/user/'));?>
-	<div class="career-bot pull-left">
-		<div class="mr0 col-md-12 fl">
-			<div class="mr0  pull-left stream-pref">
-				<h1>Tour</h1>
-				<!--<p>Watch this video to understand how you will proceed through the Gudaak journey of choosing your career through the three steps of career planning…<br />
+<div class="career-bot pull-left">
+	<div class="mr0 col-md-12 fl">
+		<div class="mr0  pull-left stream-pref">
+			<h1>Tour</h1>
+			<!--<p>Watch this video to understand how you will proceed through the Gudaak journey of choosing your career through the three steps of career planning…<br />
 Assess, Explore and Approach!</p>-->
 <!--<img src="<?php //echo Yii::app()->theme->baseUrl;?>/images/tour.png" width="100%" />-->
-
-			</div>
-			
 		</div>
-		 
-		
 	</div>
-        
-		
-		
-
-
-	
-	<div class="news  pd0 pull-right">
-		<?php  $this->Widget('WidgetNews'); ?>
-	</div>
-			
+</div>
+<div class="news  pd0 pull-right">
+	<?php  $this->Widget('WidgetNews'); ?>
+</div>
+<div id="popup_box">    <!-- OUR PopupBox DIV-->
+<img class="img_position1" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer1.png"/>
+<img class="img_position2" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer2.png"/>
+<img class="img_position3" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer3.png"/>
+</div>
+<div id="popup_box1">
+<img class="img_position4" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer4.png"/>
+<img class="img_position5" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer5.png"/>
+<img class="img_position6" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer7.png"/>
+<img class="img_position7" src="<?php echo Yii::app()->theme->baseUrl;?>/images/gudaak-pointer8.png"/>
+</div>
