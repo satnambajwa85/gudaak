23d05942dd262d2e8e7b5bc553fe99661251df1d
Satnam singh <satnam.singh@venturepact.com>
2014-07-07 15:19:00 +0530

    changes

--- themes/template/views/user/index.php
+++ themes/template/views/user/index.php
@@ -2,12 +2,12 @@
 if(isset($_REQUEST['first']) && $_REQUEST['first']==1)
 Yii::app()->clientScript->registerScript('opact','$("#popup_box").fadeIn("slow");$("#container").css({"opacity": "0.3" }); $("#popup_box").click(function(){$("#popup_box").fadeOut();$("#popup_box1").fadeIn("slow");$("#container").css({"opacity": "0.3" });});
 $("#popup_box1").click(function(){$("#popup_box1").fadeOut();});',CClientScript::POS_READY);
-$this->pageTitle=Yii::app()->name . ' - Tour';
-$this->breadcrumbs=array('Tour'=>array('/user/'));?>
+$this->pageTitle=Yii::app()->name . ' - Home';
+$this->breadcrumbs=array('Home'=>array('/user/'));?>
 <div class="career-bot pull-left">
 	<div class="mr0 col-md-12 fl">
 		<div class="mr0  pull-left stream-pref">
-			<h1>Tour</h1>
+			<h1>Home</h1>
 			<!--<p>Watch this video to understand how you will proceed through the Gudaak journey of choosing your career through the three steps of career planning…<br />
 Assess, Explore and Approach!</p>-->
 <!--<img src="<?php //echo Yii::app()->theme->baseUrl;?>/images/tour.png" width="100%" />-->
