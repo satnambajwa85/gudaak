6adf5d2d5e25db55e5e126b015f86e63406a7b3e
Satnam singh <satnam.singh@venturepact.com>
2014-07-09 11:57:51 +0530

    changes

--- themes/template/views/user/news.php
+++ themes/template/views/user/news.php
@@ -6,18 +6,15 @@ $this->breadcrumbs=array('News'=>array('/user/newsUpdates'));?>
 			<div class="mr0 col-md-12 fl artical">
 				 <h1>&nbsp;<!--Gudaak helps you to fullfill news--></h1>
 				 <h3><?php echo $news->title;?></h3>
-				 <span class="pd5">by admin</span>
-				 <div class="clear"></div>
-				 <div class="col-md-12 br-all pd0  fl">
-					<div class="col-md-3 pd10 post-info fl">
+				<div class="col-md-3 pd10 post-info fl">
+                    	by admin
 						<span>Posted on</span>
 						<datetime class="date-time fl">
 							<?php echo date('M d, Y',strtotime($news->add_date));?>
 						</datetime>
 					</div>
 					
-				 </div>
-				  <div class="clear"></div>
+				<div class="clear"></div>
 				<p class="mt15">
 				<?php echo strip_tags($news->description);?>	
 				</p>
