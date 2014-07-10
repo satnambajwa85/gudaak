e64518b8532cd320c777140b24954d8a380033d7
Satnam singh <satnam.singh@venturepact.com>
2014-07-10 11:25:27 +0530

    changes

--- themes/template/views/site/article.php
+++ themes/template/views/site/article.php
@@ -7,20 +7,14 @@ $this->breadcrumbs=array('Articles'=>array('/site/articles'),''.$articles->title
 			<div class="mr0 mt10 col-md-12 pd0 fl artical">
 				 <h1 style="font-size:20px;"><?php echo $articles->title;?></h1><br />
 				 <div class="clear"></div>
-                 <div class="col-md-3 fl pd0">
+                 <?php if(isset($articles->add_date)){?>
+                 <div class="col-md-12 fl pd0">
                     <datetime class="date-time fl">
                         Posted on <?php echo date('M d, Y',strtotime($articles->add_date));?>
                     </datetime>
                 </div>
-				 <div class="col-md-12 br-all pd0  fl">
-					<div class="col-md-3 pd0 post-info fl">
-						<span>Posted on</span>
-						<datetime class="date-time fl">
-							<?php echo Yii::app()->dateFormatter->formatDateTime(CDateTimeParser::parse($articles->add_date, 'yyyy-MM-dd'),'medium',null);?>
-						</datetime>
-					</div>
-				</div>
 				<div class="clear"></div>
+                <?php }?>
 				<p>
 				<?php echo $articles->description;?>	
 				</p>
