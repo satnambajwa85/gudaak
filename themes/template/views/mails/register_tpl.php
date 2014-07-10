922fb729a2ee2a83588addacea7329c9ce47ed86
Satnam singh <satnam.singh@venturepact.com>
2014-07-10 09:49:50 +0530

    changes

--- themes/template/views/mails/register_tpl.php
+++ themes/template/views/mails/register_tpl.php
@@ -22,29 +22,38 @@
 <table cellpadding="0" cellspacing="0" border="0" class="mail_set" width="100%">	
 	<tr>
 		<td>
-			Dear <?php echo $name; ?>,<br /><br />
+			Hi <?php echo $name; ?>,<br /><br />
 		</td>	
 	</tr>
 	<tr>
 		<td>
-			Thank you for setting up an account with Gudaak.com <br />
-            Your Credentials are as follows:
-		</td>	
+Thanks for creating your new Gudaak account.<br /><br />
+
+Your Gudaak User ID is <span><?php echo $email; ?></span><br /><br />
+<a href="<?php echo $code;?>"/>Click here</a> to active your account.<br  /><br  />
+
+Update your profile today to get started with your career planning- but before you do, we'd like to share a few tips to help you personalize and get the most out of your account.<br /><br />
+
+How to get started<br /><br />
+
+Start your Career Planning<br /><br />
+
+    Update your profile information and profile picture<br />
+    Take career tests<br />
+    Know your career recommendations<br />
+    Learn about different careers & watch career videos <br />
+    Explore and shortlist colleges & entrance exams<br />
+    Check your notifications on Gudaak for updates from us regarding upcoming online events like webinars and chats from expert panel of career counselors and professionals.<br />
+    Join our personalized career mentoring program*. Learn More  <br /><br />
+
+If you did not sign up or request a new Gudaak account, please <span style="color:#00C">contact us</span> to report the issue<br />
+We wish you all the best for your future career planning. <br /><br /><br />
+        </td>	
 	</tr>
 	<tr>
 		<td>
-			Username: <span><?php echo $email; ?></span><br />
-			Password: <span><?php echo preg_replace('/(?!^)\S/', '*', $password);?></span>
-		</td>	
-	</tr>
-	<tr>
-		<td>
-			Click here to active your account <span><a href="<?php echo $code;?>"/><?php echo $code; ?></span>
-		</td>	
-	</tr>
-	<tr>
-		<td>
-			Thanks
+			Thanks & Regards<br /><br />
+            Team Gudaak
 		</td>	
 	</tr>
 </table>
